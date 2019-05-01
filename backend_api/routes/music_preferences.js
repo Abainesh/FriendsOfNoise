e0d9498a281defd95a9f79e369cdd854df34558f
Jon <jon.king047@gmail.com>
2019-04-30 19:51:30 -0700

    added author comment

--- backend_api/routes/music_preferences.js
+++ backend_api/routes/music_preferences.js
@@ -1,10 +1,13 @@
+// Author Jon King
+// Started 4/30/19
+
 var express = require('express');
 var router = express.Router();
 
 var filename = '../dummy_json_data/music_pref.json';
 var music_preferences = require(filename);
 router.get('/', function(req, res, next) {
-  res.send(music_preferences[1]);
+  res.send(music_preferences[2]);
   
 
 

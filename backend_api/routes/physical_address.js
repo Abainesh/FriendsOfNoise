2b13336e4e3406f0c89e5a73860435fc937db831
GZSaechao <gzsaechao@gmail.com>
2019-04-30 20:02:39 -0700

    Update physical_address.js
    Adds get function to pull address data

--- backend_api/routes/physical_address.js
+++ backend_api/routes/physical_address.js
@@ -9,7 +9,26 @@ var addresses = require(filename);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Nobody here, except us chickens.");
+
+    var addressIdint = null;
+    
+    //Validates addressId
+    if(req.query.addressId === undefined || typeof req.query.addressId===undefined)
+    {
+        res.status(500).set("AddressId is required");
+    } else if (isNaN(req.query.addressId)) {
+        res.status(500).send("AddressId must be a parseable integer");
+    } else {
+      addressIdint = parseInt(req.query.addressId, 10);
+    }
+    
+    //Find if addressId exists in JSON and sends the result data if it does.
+    if(!addresses[addressIdint]){
+        res.status(500).send("Address does not exist");
+    }else {
+        res.send(addresses[addressIdint]);
+    }
+    
 });
 
 module.exports = router;

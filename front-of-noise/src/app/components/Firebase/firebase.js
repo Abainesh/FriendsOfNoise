f30bc7ae4e1f008da832c77abbddd99b4f34c573
[choisojung] <[sojc31@gmail.com]>
2019-05-03 19:50:33 -0700

    added firebase configuration file

---
+++ front-of-noise/src/app/components/Firebase/firebase.js
@@ -0,0 +1,21 @@
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/5.11.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDbc4W4kEjyqFkqwdG8fkFiqStzTPiOVPA",
+    authDomain: "friendsofnoise.firebaseapp.com",
+    databaseURL: "https://friendsofnoise.firebaseio.com",
+    projectId: "friendsofnoise",
+    storageBucket: "friendsofnoise.appspot.com",
+    messagingSenderId: "771386493067",
+    appId: "1:771386493067:web:0c92ef05a443a7c9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>

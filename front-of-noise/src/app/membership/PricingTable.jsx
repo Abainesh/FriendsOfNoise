4fb599f1273c7484a29284c6c99928f6bbeb651a
deya <30326911+miscdats@users.noreply.github.com>
2019-05-05 14:11:35 -0700

    add pricing table to membership info page

--- front-of-noise/src/app/membership/PricingTable.jsx
+++ front-of-noise/src/app/membership/PricingTable.jsx
@@ -6,24 +6,39 @@ render() {
   return (
     <div class="pricing-table is-horizontal">
   <div class="pricing-plan">
-    <div class="plan-header">Starter</div>
+    <div class="plan-header">Don't be a stranger!</div>
     <div class="plan-items">
       <div class="plan-item">Just you</div>
-      <div class="plan-item">3 shows a month</div>
-      <div class="plan-item">Discounts on merch</div>
-      <div class="plan-item">Early tickets</div>
+      <div class="plan-item">being there</div>
+      <div class="plan-item">is a bonus</div>
+      <div class="plan-item">-</div>
     </div>
     <div class="plan-footer">
-      <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>10</span>/month</div>
+      <div class="plan-price"><span class="plan-price-amount">
+      <span class="plan-price-currency">$</span>0</span>/month</div>
       <button class="button is-fullwidth" disabled="disabled">Current plan</button>
     </div>
   </div>
 
   <div class="pricing-plan is-warning">
+    <div class="plan-header">Friend</div>
+    <div class="plan-items">
+      <div class="plan-item">You!</div>
+      <div class="plan-item">Unlimited shows a month</div>
+      <div class="plan-item">Early tickets</div>
+      <div class="plan-item">Reservations</div>
+    </div>
+    <div class="plan-footer">
+      <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>10</span>/month</div>
+      <button class="button is-fullwidth">Choose</button>
+    </div>
+  </div>
+
+  <div class="pricing-plan is-danger">
     <div class="plan-header">Good Friend</div>
     <div class="plan-items">
       <div class="plan-item">You!</div>
-      <div class="plan-item">2 lucky others</div>
+      <div class="plan-item">2 Lucky others</div>
       <div class="plan-item">-</div>
       <div class="plan-item">-</div>
     </div>
@@ -34,29 +49,15 @@ render() {
   </div>
 
   <div class="pricing-plan is-active">
-    <div class="plan-header">Greater Friend</div>
-    <div class="plan-items">
-      <div class="plan-item">You!</div>
-      <div class="plan-item">5 Lucky others</div>
-      <div class="plan-item">-</div>
-      <div class="plan-item">-</div>
-    </div>
-    <div class="plan-footer">
-      <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>60</span>/month</div>
-      <button class="button is-fullwidth">Choose</button>
-    </div>
-  </div>
-
-  <div class="pricing-plan is-danger">
     <div class="plan-header">Best Friend</div>
     <div class="plan-items">
       <div class="plan-item">You!</div>
-      <div class="plan-item">10 Lucky others</div>
+      <div class="plan-item">5 Lucky others</div>
       <div class="plan-item">Your membership free!</div>
       <div class="plan-item">-</div>
     </div>
     <div class="plan-footer">
-      <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>100</span>/month</div>
+      <div class="plan-price"><span class="plan-price-amount"><span class="plan-price-currency">$</span>40</span>/month</div>
       <button class="button is-fullwidth">Choose</button>
     </div>
   </div>

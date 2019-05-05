251454e7a57e06da37c18b763a62274b7c2eef9e
deya <30326911+miscdats@users.noreply.github.com>
2019-05-05 10:22:42 -0700

    About placeholder page

---
+++ front-of-noise/src/app/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => (
+  <section className="section">
+    <div className="container">
+      <h1 className="title">Friends of Noise</h1>
+      <nav className="navbar">
+
+      </nav>
+      <p className="subtitle">
+        All ages <strong>always</strong>!
+      </p>
+      <p className="membertagline">
+        Ask about how to be sponsored for a membership.
+      </p>
+    </div>
+    <div className="col-md-6">
+      <br />
+    </div>
+  </section>
+);
+
+export default About;

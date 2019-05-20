cb891b6b07775422688c41a35a5f87e1fa84343b
deya <30326911+miscdats@users.noreply.github.com>
2019-05-19 18:12:41 -0700

    files into modularized folders

--- front-of-noise/src/App.js
+++ front-of-noise/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 // import {Helmet} from 'react-helmet';
-import 'bulma';
-import './App.css';
+import './styles/css/App.css';
 import Layout from './app/Layout'
 
 class App extends Component {

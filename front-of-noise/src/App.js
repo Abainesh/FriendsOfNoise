9f38b3165f3d562f546b02f7c72f96ba1332abb4
deya <30326911+miscdats@users.noreply.github.com>
2019-05-20 21:55:46 -0700

    test apirouting

--- front-of-noise/src/App.js
+++ front-of-noise/src/App.js
@@ -4,9 +4,35 @@ import './styles/css/App.css';
 import Layout from './app/Layout'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPIroute')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+
+
+
   render() {
     return (
-      <Layout />
+      <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+      //<Layout />
     );
   }
 }

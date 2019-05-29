f36ac4021948e1f7051335da3614a718340d08f0
Jon <jon.king047@gmail.com>
2019-05-28 19:03:32 -0700

    add firestore functionality to music_prefs

--- backend_api/routes/music_preferences.js
+++ backend_api/routes/music_preferences.js
@@ -6,12 +6,29 @@
 	Adjusted the get route, standard API is to require userId.
 	Adjusted post route, the data validation was workinng under assumption about the data - appending a new genre. We will be overwriting existing array of strings with a new array of strings.  */
 
+
+	//Initialize Firestore
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+
+// TO OBTAIN SERVICE ACCOUNT KEY go to our Firestore App > Project Settings > Service Accounts
+// Click GENERATE NEW KEY. Save this to your disk and add the file location  to the below variable
+var serviceAccount = require("/Users/kingj/OneDrive/Documents/North Seattle/AD410_Web_App_Practicum/friendsofnoise-2ced6-firebase-adminsdk-pih7o-8eaac3cb45.json");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://friendsofnoise-2ced6.firebaseio.com"
+});
+
+var db = admin.firestore();
 var express = require('express');
 var router = express.Router();
 
 // connect to the data
-var filename = '../dummy_json_data/music_pref.json';
-var music_preferences = require(filename);
+// var filename = '../dummy_json_data/music_pref.json';
+// var music_preferences = require(filename);
 
 /**
  * API definition, get music preferences given a userId
@@ -31,24 +48,45 @@ var music_preferences = require(filename);
  *   "international"
  * ]
  */
+
+
 router.get('/', function(req, res, next) {
-  var userId = req.query.userId;
-  var userIdint = null;
-
-  if(userId === undefined || typeof userId === undefined) {
-    res.status(500).send("userId required");
-  }else if(isNaN(userId)){
-    res.status(500).send("userId must be an integer")
-  }else{
-    userIdint = parseInt(userId);
-  }
-
-  if(!music_preferences[userId]){
-    res.status(500).send("user does not exist");
-  }else {
-    res.send(music_preferences[userId]);
-  }
+	
+	var userId = req.query.userId;
+	var userIdint = null;
+	var music_preferences = db.collection('user').doc(""+userId);
+	var getDoc = music_preferences.get()
+  .then(doc => {
+    if (!doc.exists) {
+			console.log('No such document!');
+			res.status(500).send("user does not exist");
+		} else {
+			res.send(doc.data());
+		}
+	})
+  .catch(err => {
+    console.log('Error getting document', err);
+	});
 });
+
+ // router.get('/', function(req, res, next) {
+//   var userId = req.query.userId;
+//   var userIdint = null;
+
+//   if(userId === undefined || typeof userId === undefined) {
+//     res.status(500).send("userId required");
+//   }else if(isNaN(userId)){
+//     res.status(500).send("userId must be an integer")
+//   }else{
+//     userIdint = parseInt(userId);
+//   }
+
+//   if(!music_preferences[userId]){
+//     res.status(500).send("user does not exist");
+//   }else {
+//     res.send(music_preferences[userId]);
+//   }
+// });
 
 /**
  * API definition, set music preferences given a userId and an array of strings
@@ -64,51 +102,107 @@ router.get('/', function(req, res, next) {
  * Note: an empty array is accepted, and will be stored
  * Success: responds with "record updated"
  */
+
+
 router.post('/', function(req, res, next){
+	var newPref = req.body.newPref;
+	toString(newPref);
 	var userId = req.body.userId;
 	var userIdint = null;
-	// expecting an array of strings for newPref
-	var newPref = req.body.new_preferences;
+	// validate userId input
 	
-	// validate userId input, convert to integer
+	console.log("starting post")
 	if (userId === undefined || typeof userId === undefined){
 		res.status(500).send("No userId specified");
-	} else if (isNaN(userId)){
-		res.status(500).send("user Id must be an integer")
+		console.log(userId);
+	} else if (newPref === undefined || typeof newPref === undefined){
+		console.log("no newPref");
+		res.status(500).send("No preference specified");
 	} else {
-		userIdint = parseInt(userId);
-	}
-	
-	// Check that userId exists
-	if(!music_preferences[userIdint]) {
-		res.status(500).send("user does not exist");
-	}
-	
-	// validate input - expecting an array of strings
-	if(!newPref.isArray()) {
-		res.status(500).send("expecting an array of strings in the new_preferences");
-	}
-	// trim any non-string pieces
-	for(var i = 0; i < newPref.length; i++) {
-		if(!typeof newPref[i] === 'string') {
-			newPref.splice(i, 1);
-			i--;
+
+		
+		
+	// check for existing user in db
+	var checkUser = db.collection('user').doc(""+userId);
+	var getDoc = checkUser.get()
+  .then(doc => {
+    if (!doc.exists) {
+			console.log('No such document!');
+			res.status(500).send("user does not exist");
+		} else {
+			console.log('good user ID')
 		}
-	}
+
+
+	// validate input - expecting an array of strings
+// 	if(!newPref.isArray()) {
+// 		res.status(500).send("expecting an array of strings in the new_preferences");
+// 	}
+// 	// trim any non-string pieces
+// 	for(var i = 0; i < newPref.length; i++) {
+// 		if(!typeof newPref[i] === 'string') {
+// 			newPref.splice(i, 1);
+// 			i--;
+// 		}
+ 	})
+};
 	
 	// Update data
-	music_preferences[userIdint] = newPref;
-	var fs = require('fs');
-	var json_format = JSON.stringify(music_preferences);
-	filename = './dummy_json_data/music_pref';
-	fs.writeFile(filename, json_format, 'utf8', (err) => {
-		if (err) {
-			res.status(500).send("fs error: " + err);
-		} else {
-			res.status(200).send("record updated");
-		}
+	var prefRef = db.collection('user').doc(""+userId).collection('prefs').doc('music_prefs');
+	var arrUnion = prefRef.update({
+		genre: admin.firestore.FieldValue.arrayUnion(""+newPref)
+		
 	});
+	console.log("database updated");
 });
+
+
+
+//  router.post('/', function(req, res, next){
+// 	var userId = req.body.userId;
+// 	var userIdint = null;
+// 	// expecting an array of strings for newPref
+// 	var newPref = req.body.new_preferences;
+	
+// 	// validate userId input, convert to integer
+// 	if (userId === undefined || typeof userId === undefined){
+// 		res.status(500).send("No userId specified");
+// 	} else if (isNaN(userId)){
+// 		res.status(500).send("user Id must be an integer")
+// 	} else {
+// 		userIdint = parseInt(userId);
+// 	}
+	
+// 	// Check that userId exists
+// 	if(!music_preferences[userIdint]) {
+// 		res.status(500).send("user does not exist");
+// 	}
+	
+// 	// validate input - expecting an array of strings
+// 	if(!newPref.isArray()) {
+// 		res.status(500).send("expecting an array of strings in the new_preferences");
+// 	}
+// 	// trim any non-string pieces
+// 	for(var i = 0; i < newPref.length; i++) {
+// 		if(!typeof newPref[i] === 'string') {
+// 			newPref.splice(i, 1);
+// 			i--;
+// 		}
+// 	}
+	
+// 	// Update data
+// 	music_preferences[userIdint] = newPref;
+// 	var fs = require('fs');
+// 	var json_format = JSON.stringify(music_preferences);
+// 	filename = './dummy_json_data/music_pref';
+// 	fs.writeFile(filename, json_format, 'utf8', (err) => {
+// 		if (err) {
+// 			res.status(500).send("fs error: " + err);
+// 		} else {
+// 			res.status(200).send("record updated");
+// 		}
+// 	});
+// });
 
 // helper function to catch empty strings and all whitespace strings
 // source: StackOverflow questionId 10232366

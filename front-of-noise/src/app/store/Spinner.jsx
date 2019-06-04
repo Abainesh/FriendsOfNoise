9a3e005cb6fd8b812977c337bde1506cfbd94e43
deya <30326911+miscdats@users.noreply.github.com>
2019-06-04 16:31:13 -0700

    store components initial upload

---
+++ front-of-noise/src/app/store/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../../styles/css/store.css'
+
+export default () => (
+  <div className="spinner lds-ring">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);

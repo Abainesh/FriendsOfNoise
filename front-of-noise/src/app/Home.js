a20a05f297ce5ec9d657a6f461c99bf1d0c65ae2
deya <30326911+miscdats@users.noreply.github.com>
2019-06-04 13:26:53 -0700

    add home page component : hero banner intro

--- front-of-noise/src/app/Home.js
+++ front-of-noise/src/app/Home.js
@@ -1,23 +1,10 @@
-import React from 'react'
+import React from 'react';
+
+import HeroHome from './home/herohome';
 
 const Home = () =>  (
-  <section className="section">
-
-    <div className="container">
-      <h1 className="title">Friends of Noise</h1>
-      <p className="subtitle">
-        All ages <strong>always</strong>!
-      </p>
-      <p className="membertagline">
-        Ask about how to be sponsored for a membership.
-      </p>
-    </div>
-
-    <div className="col-md-6">
-      <br />
-    </div>
-
-  </section>
+    <HeroHome />
+      
 
 );
 

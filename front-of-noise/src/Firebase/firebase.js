d957ce8446befea023e90c930a2eb85e4bac87cb
[choisojung] <[sojc31@gmail.com]>
2019-06-11 08:53:47 -0700

    updated firebase module in react to include firestore

--- front-of-noise/src/Firebase/firebase.js
+++ front-of-noise/src/Firebase/firebase.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+    databaseURL:"https://friendsofnoise-2ced6.firebaseio.com"
 });
 
 export default app;
